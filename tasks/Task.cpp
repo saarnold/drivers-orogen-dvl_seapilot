5f99a757b38a8a2e543af74de748cbe82cb8e751
Marc Hildebrandt <marc.hildebrandt@dfki.de>
2015-04-14 10:45:40 +0200

    removed debug cout

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -35,8 +35,6 @@ bool Task::startHook()
     if (! TaskBase::startHook())
         return false;
 
-    std::cout << _device.get() << std::endl;
-
     driver.open(_device.get());
 
     driver.startMeasurement();
@@ -50,9 +48,18 @@ void Task::updateHook()
     dvl_seapilot::Measurement measurement;
     driver.readMeasurement(measurement);
     rbs.time = base::Time::now();
-    rbs.velocity[0] = measurement.bottom_x;
-    rbs.velocity[1] = measurement.bottom_y;
+    rbs.velocity[0] = -measurement.bottom_x * sin(M_PI / 4) - measurement.bottom_y * sin(M_PI / 4);
+    rbs.velocity[1] = measurement.bottom_x * sin(M_PI / 4) - measurement.bottom_y * sin(M_PI / 4);
     rbs.velocity[2] = measurement.bottom_z;
+    rbs.cov_velocity(0, 0) = 0.0000001;
+    rbs.cov_velocity(0, 1) = 0.0000001;
+    rbs.cov_velocity(0, 2) = 0.0000001;
+    rbs.cov_velocity(1, 0) = 0.0000001;
+    rbs.cov_velocity(1, 1) = 0.0000001;
+    rbs.cov_velocity(1, 2) = 0.0000001;
+    rbs.cov_velocity(2, 0) = 0.0000001;
+    rbs.cov_velocity(2, 1) = 0.0000001;
+    rbs.cov_velocity(2, 2) = 0.0000001;
 
 //    std::cout << "HEADING: " << measurement.bottom_heading << std::endl;
 //    std::cout << "PITCH:   " << measurement.bottom_pitch << std::endl;
@@ -71,6 +78,8 @@ void Task::errorHook()
 void Task::stopHook()
 {
     TaskBase::stopHook();
+
+    driver.stopMeasurement();
 }
 void Task::cleanupHook()
 {
